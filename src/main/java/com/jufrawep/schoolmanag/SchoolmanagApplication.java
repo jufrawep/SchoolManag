3a1134a93af35acb61ab3286b30849f147b82d14
jufrawep <francjuniorepanti@gmail.com>
2024-07-26 17:02:44 +0100

    initialize School Management project

---
+++ src/main/java/com/jufrawep/schoolmanag/SchoolmanagApplication.java
@@ -0,0 +1,13 @@
+package com.jufrawep.schoolmanag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jufrawep/schoolmanag/SchoolmanagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jufrawep.schoolmanag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
